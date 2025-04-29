6fe12e17f337532feecbed8c58642975563e646c
awakha <pagalova97@mail.ru>
2025-04-29 09:10:49 +0300

    useState, part 1

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <nav className="nav">
-      <Link to="/about" className="nav__item">
+      <a to="/about" className="nav__item">
         О нас
-      </Link>
-      <Link to="/news" className="nav__item">
+      </a>
+      <a to="/news" className="nav__item">
         Новости
-      </Link>
-      <Link to="/documents" className="nav__item">
+      </a>
+      <a to="/documents" className="nav__item">
         Документы
-      </Link>
-      <Link to="/g9" className="nav__item">
+      </a>
+      <a to="/g9" className="nav__item">
         ГИА-9
-      </Link>
-      <Link to="/g11" className="nav__item">
+      </a>
+      <a to="/g11" className="nav__item">
         ГИА-11
-      </Link>
-      <Link to="/contacts" className="nav__item">
+      </a>
+      <a to="/contacts" className="nav__item">
         Контактная информация
-      </Link>
+      </a>
     </nav>
   );
 };
