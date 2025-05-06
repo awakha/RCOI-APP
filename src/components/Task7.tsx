bb16006c08bc1b83443514b509e69651df18fded
awakha <pagalova97@mail.ru>
2025-05-07 00:21:24 +0300

    useState. Practic. Finish

--- src/components/Task7.tsx
+++ src/components/Task7.tsx
@@ -1,16 +1,33 @@
 // Создайте компонент "Простой калькулятор". Создайте два поля, в одном пользователь будет вводить одно значение, а другом второе. При нажатии на кнопку в диве должен отображаться текст "Ваш ответ: " и сумма двух значений из полей ввода.
 
+import { useState } from "react";
+
 const Task7 = () => {
+  const [base, setBase] = useState(0);
+  const [increment, setIncrement] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const sum = () => {
+    setResult(Number(base) + Number(increment));
+  };
   return (
     <div>
       <div>
         <p>Первое значение</p>
-        <input type="text" />
+        <input
+          type="text"
+          value={base}
+          onChange={(e) => setBase(e.target.value)}
+        />
         <p>Второе значение</p>
-        <input type="text" />
+        <input
+          type="text"
+          value={increment}
+          onChange={(e) => setIncrement(e.target.value)}
+        />
       </div>
-      <button>Вывести сумму</button>
-      <p>Ваш ответ: </p>
+      <button onClick={sum}>Вывести сумму</button>
+      <p>Ваш ответ: {result}</p>
     </div>
   );
 };
